5bd8296e10f13d313ed24ac53b7445fc26584f6d
Tim Wagner <t.wagner@techdivision.com>
2020-12-29 15:17:12 +0100

    Merge with latest updates from 24.x branch

--- src/Observers/UrlKeyObserver.php
+++ src/Observers/UrlKeyObserver.php
@@ -20,15 +20,15 @@
 
 namespace TechDivision\Import\Product\Observers;
 
-use TechDivision\Import\Product\Utils\ConfigurationKeys;
 use Zend\Filter\FilterInterface;
 use TechDivision\Import\Utils\StoreViewCodes;
+use TechDivision\Import\Utils\UrlKeyUtilInterface;
+use TechDivision\Import\Utils\Filter\UrlKeyFilterTrait;
+use TechDivision\Import\Subjects\UrlKeyAwareSubjectInterface;
 use TechDivision\Import\Product\Utils\MemberNames;
 use TechDivision\Import\Product\Utils\ColumnKeys;
-use TechDivision\Import\Utils\Filter\UrlKeyFilterTrait;
+use TechDivision\Import\Product\Utils\ConfigurationKeys;
 use TechDivision\Import\Product\Services\ProductBunchProcessorInterface;
-use TechDivision\Import\Utils\UrlKeyUtilInterface;
-use TechDivision\Import\Subjects\UrlKeyAwareSubjectInterface;
 
 /**
  * Observer that extracts the URL key from the product name and adds a two new columns
@@ -100,59 +100,70 @@ class UrlKeyObserver extends AbstractProductImportObserver
     protected function process()
     {
 
+        // initialize the URL key and the product
+        $urlKey = null;
+        $product = array();
+
         // prepare the store view code
         $this->getSubject()->prepareStoreViewCode();
 
-        // throw an exception, that the URL key can not be initialized and we're in admin store view
-        if ($this->getSubject()->getStoreViewCode(StoreViewCodes::ADMIN) === StoreViewCodes::ADMIN
-            && $this->hasValue(ColumnKeys::URL_KEY) === false
-            && $this->hasValue(ColumnKeys::NAME) === false
-        ) {
-            throw new \Exception('Can\'t initialize the URL key because either columns "url_key" or "name" have a value set for default store view');
-        }
-
         // set the entity ID for the product with the passed SKU
-        if ($product = $this->loadProduct($this->getValue(ColumnKeys::SKU))) {
+        if ($product = $this->loadProduct($sku = $this->getValue(ColumnKeys::SKU))) {
             $this->setIds($product);
         } else {
             $this->setIds(array());
         }
 
-        // query whether or not an existing product `url_key` should recalc from product name
+        // query whether or not the URL key column has a value
         if ($this->hasValue(ColumnKeys::URL_KEY)) {
-            $urlKey =  $this->getValue(ColumnKeys::URL_KEY);
+            $urlKey = $this->getValue(ColumnKeys::URL_KEY);
         } else {
-            // product already exists and NO recalc from product key,
-            // so we search origin url_key from product
-            $urlKey = $this->loadUrlKey(
-                $this->getSubject(),
-                $this->getPrimaryKey()
-            );
+            // query whether or not the existing product `url_key` should be re-created from the product name
+            if ($product && !$this->getSubject()->getConfiguration()->getParam(ConfigurationKeys::UPDATE_URL_KEY_FROM_NAME, true)) {
+                // if the product already exists and NO re-creation from the product name has to
+                // be done, load the original `url_key`from the product and use that to proceed
+                $urlKey = $this->loadUrlKey($this->getSubject(), $this->getPrimaryKey());
+            }
 
-            // query whether or not a product name is available
-            if (!$urlKey) {
-                // initialize the URL key with the converted name
+            // try to load the value from column `name` if URL key is still
+            // empty, because we need it to process the the rewrites later on
+            if ($urlKey === null || $urlKey === '' && $this->hasValue(ColumnKeys::NAME)) {
                 $urlKey = $this->convertNameToUrlKey($this->getValue(ColumnKeys::NAME));
             }
         }
 
-        // load the URL paths of the categories
-        // found in the column `categories`
-        $urlPaths = $this->getUrlPaths();
+        // stop processing, if no URL key is available
+        if ($urlKey === null || $urlKey === '') {
+            // throw an exception, that the URL key can not be
+            // initialized and we're in the default store view
+            if ($this->getSubject()->getStoreViewCode(StoreViewCodes::ADMIN) === StoreViewCodes::ADMIN) {
+                throw new \Exception(sprintf('Can\'t initialize the URL key for product "%s" because columns "url_key" or "name" have a value set for default store view', $sku));
+            }
+            // stop processing, because we're in a store
+            // view row and a URL key is not mandatory
+            return;
+        }
 
-        // iterate over the found URL paths and try to find a unique URL key
-        for ($i = 0; $i < sizeof($urlPaths); $i++) {
-            // try to make the URL key unique for the given URL path
-            $proposedUrlKey = $this->makeUnique($this->getSubject(), $urlKey, $urlPaths[$i]);
-            // if the URL key is NOT the same as the passed one or with the parent URL path
-            // it can NOT be used, so we've to persist it temporarily and try it again for
-            // all the other URL paths until we found one that works with every URL path
-            if ($urlKey !== $proposedUrlKey) {
-                // temporarily persist the URL key
-                $urlKey = $proposedUrlKey;
-                // reset the counter and restart the
-                // iteration with the first URL path
-                $i = 0;
+        // try to load the URL paths of the categories found in the column
+        // `categories, if not available, simply make the URL key unique
+        if (sizeof($urlPaths = $this->getUrlPaths()) === 0) {
+            $urlKey = $this->makeUnique($this->getSubject(), $urlKey);
+        } else {
+            // otherwise iterate over the found URL
+            // paths and try to find a unique URL key
+            for ($i = 0; $i < sizeof($urlPaths); $i++) {
+                // try to make the URL key unique for the given URL path
+                $proposedUrlKey = $this->makeUnique($this->getSubject(), $urlKey, $urlPaths[$i]);
+                // if the URL key is NOT the same as the passed one or with the parent URL path
+                // it can NOT be used, so we've to persist it temporarily and try it again for
+                // all the other URL paths until we found one that works with every URL path
+                if ($urlKey !== $proposedUrlKey) {
+                    // temporarily persist the URL key
+                    $urlKey = $proposedUrlKey;
+                    // reset the counter and restart the
+                    // iteration with the first URL path
+                    $i = 0;
+                }
             }
         }
 
